23c595241b38c0b60aa1543bbb6f2df94d9a55b0
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-09-26 11:25:26 +0300

    first test

---
+++ compose-placeholder-test/src/main/java/com/github/boodyahmedhamdy/compose_placeholder_test/PlaceHolderCompose.kt
@@ -0,0 +1,14 @@
+package com.github.boodyahmedhamdy.compose_placeholder_test
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PlaceHolderCompose(modifier: Modifier = Modifier) {
+    Box(modifier = modifier, contentAlignment = Alignment.Center) {
+        Text("Placeholder Screen")
+    }
+}
