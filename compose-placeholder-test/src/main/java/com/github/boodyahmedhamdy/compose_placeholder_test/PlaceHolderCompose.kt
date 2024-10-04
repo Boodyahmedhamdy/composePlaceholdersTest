eaacea88b3a6a61e12c1e4cda701b9cc0f2de92f
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-10-04 10:49:35 +0300

    updated with compose compiler options

--- compose-placeholder-test/src/main/java/com/github/boodyahmedhamdy/compose_placeholder_test/PlaceHolderCompose.kt
+++ compose-placeholder-test/src/main/java/com/github/boodyahmedhamdy/compose_placeholder_test/PlaceHolderCompose.kt
@@ -6,9 +6,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
-//@Composable
-//fun PlaceHolderCompose(modifier: Modifier = Modifier) {
-//    Box(modifier = modifier, contentAlignment = Alignment.Center) {
-//        Text("Placeholder Screen")
-//    }
-//}
+@Composable
+fun PlaceHolderCompose(modifier: Modifier = Modifier) {
+    Box(modifier = modifier, contentAlignment = Alignment.Center) {
+        Text("Placeholder Screen")
+    }
+}
